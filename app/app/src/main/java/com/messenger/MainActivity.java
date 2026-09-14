2880222f4845a3f50c84b95362f3f6e7d050e55a
ivan2026-star <ivan2026-star@users.noreply.github.com>
2026-09-15 02:46:10 +0300

    switch to webview apk

---
+++ app/app/src/main/java/com/messenger/MainActivity.java
@@ -0,0 +1,21 @@
+package com.messenger;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.webkit.WebView;
+import android.webkit.WebSettings;
+import android.webkit.WebViewClient;
+
+public class MainActivity extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        WebView web = new WebView(this);
+        WebSettings s = web.getSettings();
+        s.setJavaScriptEnabled(true);
+        s.setDomStorageEnabled(true);
+        web.setWebViewClient(new WebViewClient());
+        web.loadUrl("file:///android_asset/index.html");
+        setContentView(web);
+    }
+}
